7996641a97c694a38e85388048452262b3f1168d
Hariom <82692072+DevHarry066@users.noreply.github.com>
2024-02-26 09:32:34 -0800

    Add endoints for Todo

--- backend/index.js
+++ backend/index.js
@@ -6,22 +6,31 @@ const app = express();
 app.use(express.json());
 
 
-app.get('/todos', (req, res) => {
+app.get('/todos', async (req, res) => {
     console.log("Hello, world!");
+    const todos = await todo.find();
+    console.log(todos);
+    res.status(200).json(todos);
 });
 
 
-app.post('/todo', (req, res) => {
+app.post('/todo', async (req, res) => {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
     if (!parsedPayload.success) {
         res.status(411).json({ msg: 'You send the wrong payload' });
         return;
     }
-    //Add it in MongoDB
+    // Add it in MongoDB
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false,
+    });
+    res.status(200).json({ msg: "Todo added successfully" });
 });
 
-app.put('/completed', (req, res) => {
+app.put('/completed', async (req, res) => {
     const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload);
     if (!parsedPayload.success) {
@@ -29,6 +38,13 @@ app.put('/completed', (req, res) => {
         return;
     }
     //update the task
+    await todo.update({
+        _id: req.body.id
+    },
+        {
+            completed: true,
+        });
+    res.status(201).json({ msg: 'Task updated' });
 });
 
 app.listen(3000);
